a984155d0c378f4ac69a14d3b7b0bc07dd35e928
Samuel Alawode <alawodesamuel41@gmail.com>
2023-03-28 22:05:07 +0100

    Completed the test for get_url function

--- test_project.py
+++ test_project.py
@@ -1,9 +1,9 @@
 from project import get_url, validate_url, verify_url, get_title, convert_html_to_pdf
-def test_get_url():
+def test_get_url(monkeypatch):
     # Test that function returns the correct URL
     url = "https://www.test.com"
-    user_input = lambda: url
-    assert get_url(input_function=user_input) == url
+    monkeypatch.setattr('builtins.input', lambda _:url)
+    assert get_url() == url
 
 
 
