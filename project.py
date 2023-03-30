6143ed0422b8b5e96f5b324718e9bf9d28762c54
Samuel Alawode <alawodesamuel41@gmail.com>
2023-03-30 21:23:19 +0100

    Completed the test functions.
    Updated the requirements file showing all the modules needed for the code to run effectively

--- project.py
+++ project.py
@@ -2,65 +2,14 @@ import library
 
 
 def main():
-    print(validate_url(get_url()))
-
-
-
-
-
-
-
-def get_url():
-    return input("url: ")
-
-
-def validate_url(url):
-    """
-    checks if it's a correct url format
-    """
-
-
-    pattern = library.re.compile(r'^(http(s)?://)?(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(/[a-zA-Z0-9-]+)*(\?[a-zA-Z0-9-_=]+)?')
-
-
-    # Use the pattern to match the input URL
-  
-    if not pattern.match(url):
-        validate_url(get_url())
-
-    return True
-
-def verify_url(url):
-    """
-    Checks if the url actually points to somewhere
-    """
     while True:
-        if validate_url(url):
-            try:
-                response = library.requests.head(url)
-                return response.status_code == 200
-            except:
-                return False
+        url = get_url()
+        if validate_url(url) and verify_url(url):
+            break
+        else:
+            url = get_url()
+            
 
-
-def get_title(url):
-    # Getting the webpage
-    if verify_url(url):
-        try:
-            r = library.requests.get(url)
-            soup = library.BeautifulSoup(r.content, 'html.parser')
-        except:
-            get_title(url)
-
-    try:
-        output_path = soup.title.string + ".pdf"
-    except AttributeError:
-        output_path = "file.pdf"
-    
-    return output_path
-
-        
-def convert_html_to_pdf(url):
     output_path = get_title(url)
 
     options = {
@@ -74,6 +23,47 @@ def convert_html_to_pdf(url):
     # Convert the website to a PDF file using pdfkit
     library.pdfkit.from_url(url, output_path, options=options)
 
+def get_url():
+    return input("url: ")
+
+
+def validate_url(url):
+    """
+    checks if it's a correct url format
+    """
+
+
+    pattern = library.re.compile(r'^(http(s)?)?://(www)?\.[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(/[a-zA-Z0-9-]+)*(\?[a-zA-Z0-9-_=]+)?')
+
+
+    # Use the pattern to match the input URL
+  
+    return bool(pattern.match(url))
+       
+
+def verify_url(url):
+    """
+    Checks if the url actually points to somewhere
+    """
+   
+    response = library.requests.head(url)
+    return response.status_code == 200
+           
+
+def get_title(url):
+    # Getting the webpage
+
+    r = library.requests.get(url)
+    soup = library.BeautifulSoup(r.content, 'html.parser')
+    
+    try:
+        output_path = soup.title.string.replace(" ", "_") + ".pdf"
+    except AttributeError:
+        output_path = "file.pdf"
+    
+    return output_path
+
+        
 
 
 if __name__=='__main__':
--- test_project.py
+++ test_project.py
@@ -1,20 +1,23 @@
-from project import get_url, validate_url, verify_url, get_title, convert_html_to_pdf
+from project import get_url, validate_url, verify_url, get_title
 def test_get_url(monkeypatch):
     # Test that function returns the correct URL
-    url = "https://www.test.com"
+    url = "https://www.example.com"
     monkeypatch.setattr('builtins.input', lambda _:url)
     assert get_url() == url
 
 
 
 def test_validate_url():
-    ...
+    assert validate_url("example.com") == False
+    assert validate_url("www.example.com") == False
+    assert validate_url("http://www.example.com") == True
+    assert validate_url("https://www.example.com") == True
+    assert validate_url(".example.com") == False
+    
 
 def test_verify_url():
-    ...
+    assert verify_url("http://www.example.com") == True
+
 
 def test_get_title():
-    ...
-
-def test_convert_html_to_pdf():
-    ...
+    assert get_title("http://www.example.com") == "Example_Domain.pdf"
